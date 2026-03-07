5712ca5900446b18d85e20e5064dcb2f1f144eb2
Michael Wehle <michael.wehle@mt-ag.com>
2026-03-07 21:39:56 +0100

    Simplify visualize public API aliases

--- src/datapreprocessor/visualize/__init__.py
+++ src/datapreprocessor/visualize/__init__.py
@@ -1,4 +1,4 @@
-from .flaw_report_plot import run
 from .flaw_report_plot import run as flaws
-from .pair_length_hist_plot import run
 from .pair_length_hist_plot import run as pairs
+
+__all__ = ["flaws", "pairs"]
